b140564f3e5921efe7ececb628ccf405e6002794
Kleo Hasani <64975970+KleoHasani@users.noreply.github.com>
2022-03-09 18:04:58 -0500

    added flags

--- src/psst/logo.py
+++ src/psst/logo.py
@@ -5,7 +5,11 @@ def draw_logo() -> None:
     cat_row_2 = "(,-.`._,'(       |\`-/|"
     cat_row_3 = "    `-.-' \ )-`( , o o)"
     cat_row_4 = """          `-    \`_`"'-"""
+
+    # Clear terminal screen.
     clear()
+
+    # Print logo.
     echo(f'{style("-" * 48, fg="blue")}')
     echo(f'{style("#", fg="blue")}{" " * 46}{style("#", fg="blue")}')
     echo(f'{style("#", fg="blue")}{" " * 20} PSST {" " * 20}{style("#", fg="blue")}')
--- src/psst/main.py
+++ src/psst/main.py
@@ -1,20 +1,34 @@
-from socket import SOCK_DGRAM, SOCK_STREAM
-from click import argument, group
+from socket import SOCK_DGRAM, SOCK_STREAM, AF_INET, AF_INET6
+from click import argument, group, option
+
 from .logo import draw_logo
 from .pprinter import p_print
 from .scan import p_scan
 from .Target import Target
 
 @group(invoke_without_command=True)
+@option("-i", default=False, is_flag=True, help="Is IPv6?")
+@option("-u", default=False, is_flag=True, help="Is UDP?")
+@option("-p", default=False, is_flag=True, help="Try all ports? (1 - 65353)")
 @argument('remote_address', default="127.0.0.1")
-def cli(remote_address):
+def cli(remote_address, i, u, p):
     # Print logo.
     draw_logo()
 
-    # Scan target and Print TCP ports.
-    p_print(p_scan(Target(remote_address, [i for i in range(1, 65535)], SOCK_STREAM)))
+    ip_fam = AF_INET
+    ip_type = SOCK_STREAM
+    port_range = [i for i in range(1, 10000)]
 
-    # Scan target and Print UDP ports.
-    p_print(p_scan(Target(remote_address, [i for i in range(1, 65535)], SOCK_DGRAM)))
+    if i:
+        ip_fam = AF_INET6
+    
+    if u:
+        ip_type = SOCK_DGRAM
+    
+    if p:
+        port_range = [i for i in range(1, 65535)]
+
+    # Scan target and Print TCP ports.
+    p_print(p_scan(Target(remote_address, port_range, ip_fam, ip_type)))
 
     pass
