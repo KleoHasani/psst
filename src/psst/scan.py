ef37723952b26735b7dce983d5f0295651eff976
Kleo <>
2022-03-10 09:14:35 -0500

    fixed connection

--- src/psst/scan.py
+++ src/psst/scan.py
@@ -11,7 +11,7 @@ def connect(add: str, port: int) -> bool:
     con = soc.connect_ex((add, port))
         
     if not con == 0:
-    	is_open = False
+        is_open = False
     
     # Close connection.
     soc.close()
