6ea336bbff1976e6b5f96ead441913245547d5e9
Kleo <>
2022-03-10 09:13:56 -0500

    cleaned up socket connection

--- src/psst/scan.py
+++ src/psst/scan.py
@@ -2,30 +2,31 @@ import socket
 from .Target import Target
 
 # Attempt Socket connection.
-def connect(soc: socket.socket, add: str, port: int) -> bool:
+def connect(add: str, port: int) -> bool:
     is_open = True
+    
+    soc = socket.socket()
 
     # Connect.
-    try:
-        soc.connect((add, port))
-    except:
-        is_open = False
+    con = soc.connect_ex((add, port))
+        
+    if not con == 0:
+    	is_open = False
     
     # Close connection.
     soc.close()
     return is_open
+
 
 # Perform scan.
 def p_scan(target: Target) -> list:
     # Open ports list.
     open_ports: list = []
 
-    # Create socket object.
-    soc: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ip_add = socket.gethostbyname(target.address)
 
     for port in target.ports:
-        if connect(soc, ip_add, port):
+        if connect(ip_add, port):
             open_ports.append(port)
             
     return open_ports
